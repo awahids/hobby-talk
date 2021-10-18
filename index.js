032d0673ba205f53fc437f47bd2b97ea50ddea28
mfirliismail <m.firliismail@gmail.com>
2021-10-18 11:25:05 +0700

    ! fix cors

--- index.js
+++ index.js
@@ -3,25 +3,22 @@ const app = express()
 const port = process.env.PORT || 5000
 const router = require('./routes')
 const db = require('./db/database')
-
 const passport = require('./middlewares/passport')
 const session = require('cookie-session')
 const cors = require('cors')
-app.use(
-    cors({
-        origin: "http://localhost:3000", // <-- location of the react app were connecting to
-        credentials: true,
-    })
-    )
-    app.use(express.json())
+
+
+app.use(cors())
+app.use(express.json())
 db()
 app.use(session({
     name: "userlogin-cookie",
     keys: ["apaaja", "bisaapaaja"]
 }))
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(cors(), passport.initialize())
+app.use(cors(), passport.session())
 app.use('/api/v1', router)
+
 
 app.get('/', (req, res) => {
     return res.status(200).json({
@@ -29,7 +26,6 @@ app.get('/', (req, res) => {
         message: "server connected"
     })
 })
-
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`)
